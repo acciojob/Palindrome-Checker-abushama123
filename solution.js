6c1edf3bb4ea8048efc52dfaff0f08c23fd00eb8
Abushama Siddique <abushamasiddique175@gmail.com>
2022-07-16 20:03:34 +0530

    Assignment Done

--- solution.js
+++ solution.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
-
+    var ans ="";
+    for(var i = str.length; i >=0; i--){
+        var c = str.charAt(i);
+        ans = ans + c;
+    }
+    let result = ans.toLowerCase();
+    if(result === str.toLowerCase()){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 module.exports = palindrome
